e4429c9eaa7246d3e5287e1177ae662e86833556
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-01-10 19:41:42 +0530

    Removed unwanted files and decluttered repository

---
+++ accounts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+# Register your models here.
+admin.site.register(User, CustomUserAdmin)
