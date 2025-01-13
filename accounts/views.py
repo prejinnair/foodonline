5086e1486bd550c425fa703c9e4dc1dfcd3f2e47
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-01-13 11:13:52 +0530

    Added accounts app for user registration, modified user table

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def register_user(request):
+    return HttpResponse('This is register page')
