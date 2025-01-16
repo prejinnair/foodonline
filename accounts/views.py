ca41870f300f96e65b2e0745da65f6c55a40c5d5
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-01-16 20:53:54 +0530

    Validations and user_save
    Field level validations and error functionalities are done

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-
+from .models import User
 # Create your views here.
 def register_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.set_password(password)
+            user.save()
             return redirect('register_user')
     else:
         form = UserForm()
