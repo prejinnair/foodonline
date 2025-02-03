07a523c0e9c0a71efabd807c6442601010efa30d
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-02-03 21:14:44 +0530

    Introduced Google Api keys

--- foodonline/settings.py
+++ foodonline/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -150,3 +151,5 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
