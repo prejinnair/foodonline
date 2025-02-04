0dca3bd9c3f5d558d6059e8c061a3c34560fc5fb
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-02-04 20:58:42 +0530

    Update custom.js
    Completed google maps api integration for vendor profile edit page

--- foodonline/static/js/custom.js
+++ foodonline/static/js/custom.js
@@ -22,5 +22,40 @@ function onPlaceChanged (){
     else{
         console.log('place name=>', place.name)
     }
-    // get the address components and assign them to the fields
+
+    var geocoder = new google.maps.Geocoder()
+    var address = document.getElementById('id_address').value
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+            $('#id_latitude').val(lat)
+            $('#id_longitude').val(lng)
+            $('#id_address').val(address)
+        }
+    })
+    for (var i = 0; i < place.address_components.length; i++) {
+        for (var j = 0; j < place.address_components[i].types.length; j++) {
+            // get country
+            if (place.address_components[i].types[j] == "country") {
+                $('#id_country').val(place.address_components[i].long_name)
+            }
+            // get state
+            if (place.address_components[i].types[j] == "administrative_area_level_1") {
+                $('#id_state').val(place.address_components[i].long_name)
+            }
+            // get city
+            if (place.address_components[i].types[j] == "administrative_area_level_3") {
+                $('#id_city').val(place.address_components[i].long_name)
+            }
+            // get pincode
+            if (place.address_components[i].types[j] == "postal_code") {
+                $('#id_pin_code').val(place.address_components[i].long_name)
+            }
+            else{
+                $('#id_pin_code').val('')
+
+            }
+        }
+    }
 }
