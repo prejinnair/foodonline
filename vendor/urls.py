f62b5df3199c5d9e78521a248b281b718e297961
Prejin Nair <89181239+prejinnair@users.noreply.github.com>
2025-02-21 21:24:13 +0530

    add category done

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', accounts_views.my_account, name='vendor'),
     path('profile/', views.vendor_profile, name='vendor_profile'),
     path('menu_builder/', views.menu_builder, name='menu_builder'),
-    path('menu_builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category')
-
+    path('menu_builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
+    #Category CRUD
+    path('menu_builder/category/add/', views.add_category, name='add_category'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import user_passes_test, check_role_vendor
 from menu.models import Category, FoodItem
+from menu.forms import CategoryForm
+from django.template.defaultfilters import slugify
 # Create your views here.
 
 def get_vendor(request):
@@ -62,3 +64,21 @@ def fooditems_by_category(request, pk=None):
     }
     print(food_items, 'food_items')
     return render(request, 'vendor/fooditems_by_category.html', context)
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            category.save()
+            messages.success(request, 'Category added successfully!. ')
+            return redirect('menu_builder')
+    else:
+        form = CategoryForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'vendor/add_category.html', context)
